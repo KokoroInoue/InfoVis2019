d615e276a0dd9437b59d619d2a2d5dd8e81c872f
KokoroInoue <1645039t@stu.kobe-u.ac.jp>
2019-04-16 01:04:32 +0900

    task02

--- W02/vec3.js
+++ W02/vec3.js
@@ -48,3 +48,22 @@ Vec3.prototype.mid = function(){
     return mid;
 }
 
+//distance
+Vec3.prototype.dis = function(v){
+    a = this.x - v.x;
+    b = this.y - v.y;
+    c = this.z - v.z;
+    return(Math.sqrt((a * a) + (b * b) + (c * c)));
+}
+
+//tyuten
+Vec3.prototype.midpoint = function(v){
+    return new Vec3((this.x + v.x)/2, (this.y + v.y)/2, (this.z + v.z)/2);
+}
+
+Vec3.prototype.AreaOfTriangle = function(vi, vj){
+    var teihen = this.dis(vi);
+    var takasa = (this.midpoint(vi)).dis(vj);
+    return (teihen * takasa)/2;
+}
+
