1a1cc86dbfe14fa800efc3c4c266982a4d6ce921
KokoroInoue <1645039t@stu.kobe-u.ac.jp>
2019-04-15 21:20:48 +0900

    task01

--- W02/vec3.js
+++ W02/vec3.js
@@ -17,3 +17,34 @@ Vec3.prototype.add = function(v){
 Vec3.prototype.sum = function(){
     return this.x + this.y + this.z;
 }
+
+//Min method
+Vec3.prototype.min = function(){
+    min = this.x;
+    if(min > this.y){
+	min = this.y;
+    }
+    if(min > this.z){
+	min = this.z;
+    }
+    return min;
+}
+
+//Max method
+Vec3.prototype.max = function(){
+    max = this.x;
+    if(max < this.y){
+	max = this.y;
+    }
+    if(max < this.z){
+	max = this.z;
+    }
+    return max;
+}
+
+//Mid method
+Vec3.prototype.mid = function(){
+    mid = this.sum() - this.min() - this.max();
+    return mid;
+}
+
